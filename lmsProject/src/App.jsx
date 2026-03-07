0dcd3aca132dbd8c8c37dec18bad2fe9788772dc
madhavareddyrb <madhavareddyrb@gmail.com>
2026-03-07 21:06:10 +0530

    Implented Course Crads to display all courses

--- lmsProject/src/App.jsx
+++ lmsProject/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import CourseCards from './Components/CourseCards/CourseCards'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
+      <CourseCards/>
     </>
   )
 }
