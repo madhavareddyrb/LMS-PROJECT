fad4375465fb3b8d0668d6e77ec0f9bdabcac2b1
madhavareddyrb <madhavareddyrb@gmail.com>
2026-03-24 19:14:00 +0530

    add to cart functionality

--- lmsProject/src/Components/HomePageComponents/HomePage/HomePage.jsx
+++ lmsProject/src/Components/HomePageComponents/HomePage/HomePage.jsx
@@ -11,5 +11,6 @@ export default function HomePage(){
   <TopCategories/>
   <WhyLearnWithUs/>
   <FeaturedCourses/>
+  
   </>)
 }
---
+++ lmsProject/src/features/counter/AddToCartSlice.js
@@ -0,0 +1,25 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState   = {
+//   value: "Empty Cart",
+// };
+
+// export const addToCartSlice = createSlice({
+//   name: "addtocart",
+//   initialState,
+//   reducers: {
+//     addItem: (state) => {
+//       state.value += 1;
+//     },
+//     removeItem: (state) => {
+//       state.value -= 1;
+//     },
+//     deleteAll: (state) => {
+//       state.value = "Empty Cart";
+//     },
+//   },
+// });
+
+// export const { addItem, removeItem, deleteAll } = addToCartSlice.actions;
+
+// export default addToCartSlice.reducer;
