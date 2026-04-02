007c72ef2b61449969ebece28f4c462d6c6b19f1
madhavareddyrb <madhavareddyrb@gmail.com>
2026-04-02 23:31:22 +0530

    Created backend server

---
+++ LMS_Project_Backend/server.js
@@ -0,0 +1,52 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/" && req.method === "GET") {
+//     return res.end({
+//       message: "This is my first server",
+//     });
+//   }
+// });
+
+// why express sending header, creating server for all apis is not good
+
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const { cp } = require("fs");
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: "*", // allow any frontend
+  methods: ["GET", "POST", "PUT", "DELETE"], // allow these methods
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  return res.send("Hi madhava");
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  // console.log(req.body);
+  if (email === "Madhava" && password === "12345") {
+    return res.json({
+      email: email,
+      password: password,
+      message: "user login sucess",
+    });
+  } else {
+    console.log("worng credentials");
+  }
+});
+
+// app.get("/man", (req, res) => {
+//   return res.send("This is man paex");
+// });
+
+app.listen(3000, () => {
+  console.log("server started");
+});
